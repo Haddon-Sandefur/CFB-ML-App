88d3c6f13b30a9976620b89c6f3c4d708cf78035
Haddon <99365857+Haddon-Sandefur@users.noreply.github.com>
2023-08-13 00:32:31 -0400

    wip of shiny app....looking better

--- cfbapp/app.R
+++ cfbapp/app.R
@@ -26,44 +26,84 @@ dfl <- dfl %>% select(school, opponent, week, matches("AvgLag"))
 newNames <- snakecase::to_any_case(colnames(dfl), case = "snake") 
 newNames <- gsub("_lag", "", newNames)
 newNamesLimited <- sort(newNames[-c(1,2,3)])
-teamNames <- sort(dfl$school)
+teamNames <- sort(unique(dfl$school))
 source("predict matchups.R")
 
 # Create the Shiny UI and Server components
 ui <- fluidPage(
+  
   theme = bs_theme(
-    bg = "#6BF3A7", fg = "#050505", primary = "#9E0092",
+    bg = "#A3E38B", fg = "#050505", primary = "#198E42",
     base_font = font_google("Space Mono"),
     code_font = font_google("Space Mono")
   ),
   
-  tags$head(
-    tags$style(HTML("
-                      #vis1 {
-                        border: 2px solid black;
-                      }
-                      "))
-  ),
+  tags$style(HTML("
+      @import url('https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap');
+      h1 {
+        font-family: 'Space Mono', sans-serif;
+        color:#767676  ;
+        font-size: 50px;
+      }
+      h2 {
+        font-family: 'Space Mono', sans-serif;
+        color:#32BD62  ;
+        font-size: 50px;
+      }
+      body {
+        background-color: #FBF2DC;
+        color: #0D9347;
+        background: repeating-linear-gradient(
+          to bottom,
+          #FBF2DC 0px,
+          #DAD2BF 20px,
+          #FBF2DC 20px,
+          #DAD2BF 40px,
+        );
+      }
+      .shiny-input-container {
+        color: #474747;
+      }
+      p {
+        color: #050505;
+        border-color: #A3E38B;
+        border-bottom-style: solid;
+        font-size: 20px;
+      }
+      code {
+         font-size: 20px;
+      }
+      
+      
+}")),
   
-  titlePanel("College Football Prediction Model"),
+  h1("College Football Matchup Predictor"),
+  h5('Please click the "Submit" button to get started!'),
   sidebarLayout(
     sidebarPanel(
-      selectizeInput("team1", "School",   choices = teamNames, multiple = FALSE),
-      selectizeInput("team2", "Opponent", choices = teamNames, multiple = FALSE),
-      numericInput("spread", "Team 1's Spread", value = -7, min = -80, max = 80),
+      selectizeInput("team1", "Home Team",   choices = teamNames, multiple = FALSE, selected = teamNames[27]),
+      selectizeInput("team2", "Away Team", choices = teamNames, multiple = FALSE, selected = teamNames[36]),
+      numericInput("spread", "Home Team's Spread", value = -7, min = -80, max = 80),
       numericInput("moneyLine1", "School's Moneyline",value = -500, min = -12000, max = 12000),
       numericInput("moneyLine2", "Opponent's Moneyline",value = 350, min = -12000, max = 12000),
       numericInput("overUnder",  "Over/Under",value = 90, min = 0, max = 180),
       selectizeInput("plotVar", "X-Axis Variable", choices = newNamesLimited, multiple = FALSE,
                      selected = newNamesLimited[74]),
-      actionButton("submitBtn", "Get Predictions")
+      actionButton("submitBtn", "Submit")
     ),
     mainPanel(
       tableOutput("predictionsTable"),
-      plotOutput("comparePlot")
+      plotOutput("comparePlot"),
+      code("Info:"),
+      p("The predictions you see use an XGBoost model for the output. Overall, the accuracy of wins/losses sits at about 78%, whereas the
+        accuracey of the Cover Prediction sits at about 58%. The model was trained up until the last 2-3 games of the 2022 Season for all teams!")
     ),
+    
    
-  )
+  ),
+  br(),
+  br(),
+  fluidRow(column(1, img(src="cute.png", align = "bottom", height='10',width='10')))
 )
 
 server <- function(input, output) {
@@ -117,7 +157,7 @@ server <- function(input, output) {
             ungroup() %>% 
             set_names(newNames) %>% 
             ggplot(aes_string(y= "school", x = variable)) +
-            geom_col(aes(color = school, fill = school), size = 2) +
+            geom_col(aes(color = school, fill = school), linewidth = 2) +
             scale_color_cfb(alt_colors = teamNames) +
             scale_fill_cfb(alpha = .8) +
             theme_minimal() +
@@ -141,6 +181,7 @@ server <- function(input, output) {
   output$comparePlot <- renderPlot(
     expr = {cfbplot()},  bg="transparent"
   )
+  
 }
 # Run the application 
 shinyApp(ui = ui, server = server)
